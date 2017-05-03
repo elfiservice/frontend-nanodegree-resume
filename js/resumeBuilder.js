d3bcbc66f3b7826820fef6222bff09e2896df272
elfiservice <armandojunior@Armandos-MacBook-Pro.local>
2017-05-03 10:01:29 -0300

    add: exemplo .replace and append

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,19 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ var myName = "Armando Jr.";
+ var myRole = "Front-End Dev.";
+ // $("#main").append(myName);
+ //
+ // var awesomeThoughts = "I am " + myName + " and I am AWESOME!";
+ //
+ // console.log(awesomeThoughts);
+ //
+ // //String replace
+ // var funThoughts = awesomeThoughts.replace("AWESOME","FUN");
+ // $("#main").append(funThoughts);
+
+ var formattedName = HTMLheaderName.replace("%data%",myName);
+ var formattedRole = HTMLheaderRole.replace("%data%",myRole);
+ $("#header").prepend(formattedRole);
+ $("#header").prepend(formattedName);
